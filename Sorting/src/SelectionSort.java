6581fc0261a047edd6ca1ab2f412071e26a12c05
Anjali patil <anjupatil42@gmail.com>
2022-09-21 14:46:10 +0530

    selection sort

--- Sorting/src/SelectionSort.java
+++ Sorting/src/SelectionSort.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class SelectionSort {
+
+import java.util.*;
+import java.io.*;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        selectionSort(a);
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+
+    }
+    public static void selectionSort(int[] a)
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            int last=a.length-i-1;
+            int max=getMax(a,0,last);
+            swap(a,max,last);
+        }
+    }
+
+    public static int getMax(int[] a,int start,int last)
+    {
+        int max=start;
+        for(int i=start;i<=last;i++)
+        {
+            if(a[max]<a[i])
+            {
+                max=i;
+            }
+        }
+        return max;
+    }
+    public static void swap(int[] a,int first,int second)
+    {
+        int temp=a[first];
+        a[first]=a[second];
+        a[second]=temp;
+    }
 }
